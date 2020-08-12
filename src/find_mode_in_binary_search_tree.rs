16e445d3bc5978bce158a8d7a3df5d280bbbfcf5
navyd <dhjnavyd@gmail.com>
2020-08-12 12:49:11 +0800

    second submission

--- src/find_mode_in_binary_search_tree.rs
+++ src/find_mode_in_binary_search_tree.rs
@@ -7,13 +7,13 @@ pub mod solution_dfs {
     ///
     /// hashmap保存2个以上的值，
     /// 找出时用两次遍历先找max count，再用count去找val
-    /// 
+    ///
     /// ## Submission
-    /// 
+    ///
     /// date=20200809, mem=2.9, mem_beats=100, runtime=4， runtime_beats=100, url=https://leetcode-cn.com/submissions/detail/96161879/
-    /// 
+    ///
     /// ## 复杂度
-    /// 
+    ///
     /// - 时间：O(N)
     /// - 空间：O(N)
     pub struct Solution;
@@ -52,18 +52,18 @@ pub mod solution_dfs {
 }
 
 /// 总结
-/// 
+///
 /// 有一个初始版，没有弄清该方法的原理，一直都无法通过，反复在中序parent=last_root,
 /// pre是什么纠结，调试发现` cur_count: &mut u32, max_count: &mut u32,`有无用引用
 /// 有区别，很难受，[1,1]这个测试无法通过。
-/// 
+///
 /// ```ignore
 /// fn inorder(
 ///     root: Option<Rc<RefCell<TreeNode>>>,
 ///     parent: Option<Rc<RefCell<TreeNode>>>,
 ///     cur_count: &mut u32,
 ///     max_count: &mut u32,
-///     res: &mut Vec<i32>,
+///     modes: &mut Vec<i32>,
 /// ) {
 ///     if let Some(root) = root {
 ///         Self::inorder(
@@ -71,15 +71,15 @@ pub mod solution_dfs {
 ///             Some(root.clone()),
 ///             cur_count,
 ///             max_count,
-///             res,
+///             modes,
 ///         );
 ///     }
 /// }
 /// ```
-/// 
+///
 /// 一直都没有数组转tree的方法调试，用了点时间写了个helper，
 /// 后面用本地debug才发现什么区别的，靠
-/// 
+///
 /// rust还是很生疏的
 pub mod solution_dfs_follow_up {
     use super::*;
@@ -87,95 +87,88 @@ pub mod solution_dfs_follow_up {
     /// # 思路
     ///
     /// 递归中比较前驱节点pre与当前节点，找cur_count, max_count，表示一个val出现次数
-    /// 
+    ///
     /// - 如果`pre_node.val==cur_node.val`则cur_count += 1 表示还是之前的val
     /// - 如果`pre_node.val!=cur_node.val`则cur_count = 1 表示新开始一个val
-    /// 
+    ///
     /// 在新node更新时当
-    /// 
-    /// - `cur_count == max_count`：表示是不同的值出现数量一样，加入res中。由于
+    ///
+    /// - `cur_count == max_count`：表示是不同的值出现数量一样，加入modes中。由于
     /// 题目定义相等值必然会连续出现在子树中，一旦是不同值则会中断cur_count
-    /// - `cur_count > max_count`：表示之前的res不正确，当前val出现更多
-    /// 
+    /// - `cur_count > max_count`：表示之前的modes不正确，当前val出现更多
+    ///
     /// ## 注意
-    /// 
+    ///
     /// 该方法主要还是inorder，其中pre, cur_count, max_count都是引用，在整个
     /// 递归过程中不能被回溯时值覆盖已改变的值。
-    /// 
+    ///
     /// 如果是用传值的方式则会导致回溯时之前max_count, pre回到之前状态
-    /// 
+    ///
     /// ```ignore
     /// fn inorder(
     ///     root: Option<Rc<RefCell<TreeNode>>>,
-    ///     res: &mut Vec<i32>,
-    ///     // 传值
+    ///     modes: &mut Vec<i32>,
+    ///     // 传值 不可用！！
     ///     pre: Option<Rc<RefCell<TreeNode>>>,
     ///     cur_count: u32,
     ///     max_count: u32,
     /// ) {
     /// ```
-    /// 
+    ///
     /// 如果要用java的方式对primitive传值，可在struct中作为字段保证不会回溯更改
-    /// 
-    /// ## Submission
-    /// 
+    ///
+    /// ## Submissions
+    ///
     /// date=20200811, mem=3.1, mem_beats=100, runtime=4, runtime_beats=100, url=https://leetcode-cn.com/submissions/detail/96889428/
     /// 
+    /// date=20200812, mem=3, mem_beats=100, runtime=4, runtime_beats=66.67, url=https://leetcode-cn.com/submissions/detail/97226821/
+    ///
     /// author=junstat, references=https://leetcode-cn.com/problems/find-mode-in-binary-search-tree/solution/er-cha-sou-suo-shu-zhong-de-zhong-shu-by-junstat/
-    /// 
+    ///
     /// ## 复杂度
-    /// 
-    /// - 时间：O(N)。如果树结点val在后面出现一次多一次，导致res.clear()频繁更新，用时更多
+    ///
+    /// - 时间：O(N)。如果树结点val在后面出现一次多一次，导致modes.clear()频繁更新，用时更多
     /// - 空间：O(log N)
     pub struct Solution;
 
     impl Solution {
         pub fn find_mode(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
-            let mut res = vec![];
-            Self::inorder(root, &mut None, &mut 1, &mut 0, &mut res);
-            res
+            let mut modes = vec![];
+            Self::find_mode_by_dfs_inorder(root, &mut None, &mut 1, &mut 0, &mut modes);
+            modes
         }
 
-        fn inorder(
+        fn find_mode_by_dfs_inorder(
             root: Option<Rc<RefCell<TreeNode>>>,
             pre: &mut Option<Rc<RefCell<TreeNode>>>,
             cur_count: &mut u32,
             max_count: &mut u32,
-            res: &mut Vec<i32>,
+            modes: &mut Vec<i32>,
         ) {
             if let Some(root) = root {
                 // pre在第一个叶子结点left时为None，
                 // 后面一直都是上个root
-                Self::inorder(
-                    root.borrow().left.clone(),
-                    pre,
-                    cur_count,
-                    max_count,
-                    res,
-                );
+                Self::find_mode_by_dfs_inorder(root.borrow().left.clone(), pre, cur_count, max_count, modes);
                 let root_val = root.borrow().val;
+                // 连续的相等的node
                 if let Some(pre) = pre {
                     if pre.borrow().val == root_val {
                         *cur_count += 1;
-                    } else {
+                    } 
+                    // 新node
+                    else {
                         *cur_count = 1;
                     }
                 }
                 if cur_count == max_count {
-                    res.push(root_val);
+                    modes.push(root_val);
                 } else if cur_count > max_count {
-                    res.clear();
-                    res.push(root_val);
+                    modes.clear();
+                    modes.push(root_val);
                     *max_count = *cur_count;
                 }
                 *pre = Some(root.clone());
-                Self::inorder(
-                    root.borrow().right.clone(),
-                    pre,
-                    cur_count,
-                    max_count,
-                    res,
-                );
+                Self::find_mode_by_dfs_inorder(root.borrow().right.clone(), pre, cur_count, max_count, modes);
             }
         }
     }
@@ -196,7 +189,10 @@ mod tests {
     fn basic_solution_dfs_follow_up() {
         use solution_dfs_follow_up::Solution;
         assert_eq!(vec![2], Solution::find_mode(convert_tree("[1,null,2,2]")));
-        assert!(is_included_vec(&vec![1,2], &Solution::find_mode(convert_tree("[1,2]"))));
+        assert!(is_included_vec(
+            &vec![1, 2],
+            &Solution::find_mode(convert_tree("[1,2]"))
+        ));
         assert_eq!(vec![1], Solution::find_mode(convert_tree("[1,1]")));
         assert_eq!(vec![-2], Solution::find_mode(convert_tree("[-2,-2,-2]")));
     }
