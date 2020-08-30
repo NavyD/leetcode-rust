45f8468da024ef1f6d29d6dfac9e3810eb70cbef
navyd <dhjnavyd@gmail.com>
2020-08-30 12:45:54 +0800

    remove_duplicates_from_sorted_array first, reverse_nodes_in_k_group second

---
+++ src/remove_duplicates_from_sorted_array.rs
@@ -0,0 +1,93 @@
+pub mod solution_double_pointer {
+    /// # 思路
+    /// 
+    /// 数组是有序的，重复的元素一定会相邻，双指针将不重复的元素移到数组的左侧。
+    ///
+    /// ```java
+    /// @Submission(date = "20200830", memory = 41.7, memoryBeatRate = 30, runtime = 1, runtimeBeatRate = 94, url = "https://leetcode-cn.com/submissions/detail/102996457/")
+    /// public int removeDuplicates(int[] nums) {
+    ///     int i = 0;
+    ///     for (int j = 1; j < nums.length; j++) {
+    ///         if (nums[i] != nums[j]) {
+    ///             nums[++i] = nums[j];
+    ///         }
+    ///     }
+    ///     return i + 1;
+    /// }
+    /// ```
+    /// 
+    /// 参考：
+    /// 
+    /// - [【双指针】删除重复项-带优化思路](https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/solution/shuang-zhi-zhen-shan-chu-zhong-fu-xiang-dai-you-hu/)
+    /// 
+    /// ***不明白为何rust是4ms时间，而java是1ms，而且rust有0ms的方法不明白有什么不一样***
+    /// 
+    /// rust std有个和这个相同功能的API：[Vec::dedup](https://doc.rust-lang.org/std/vec/struct.Vec.html#method.dedup)，
+    /// 提交耗时也用了4ms。。。
+    /// 
+    /// ```ignore
+    /// pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+    ///     nums.dedup();
+    ///     nums.len() as i32
+    /// }
+    /// ```
+    ///
+    /// ### Submissions
+    ///
+    /// date=20200830, mem=2.3, mem_beats=53.49, runtime=4, runtime_beats=30, url=https://leetcode-cn.com/submissions/detail/103003211/
+    /// 
+    /// ### 复杂度
+    /// 
+    /// - 时间：O(n)
+    /// - 空间：O(1)
+    pub struct Solution;
+
+
+    impl Solution {
+        pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+            if nums.len() == 0 {
+                return 0;
+            }
+            let mut i = 0;
+            for j in 1..nums.len() {
+                if nums[i] != nums[j] {
+                    i += 1;
+                    nums[i] = nums[j];
+                }
+            }
+            return i as i32 + 1;
+        }
+
+        /// [来源](https://leetcode-cn.com/submissions/api/detail/26/rust/0)
+        #[allow(dead_code)]
+        fn remove_duplicates_0ms(nums: &mut Vec<i32>) -> i32 {
+            if nums.len() < 1 {
+                return 0;
+            }
+            let mut tail = 1;
+            let mut las = nums[0];
+            for i in 1..nums.len() {
+                if nums[i] != las {
+                    las = nums[i];
+                    nums[tail] = nums[i];
+                    tail += 1;
+                }
+            }
+            tail as i32
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        #[test]
+        fn basics() {
+            assert_eq!(
+                5,
+                Solution::remove_duplicates(&mut vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4])
+            );
+            assert_eq!(2, Solution::remove_duplicates(&mut vec![1, 1, 2]));
+            assert_eq!(0, Solution::remove_duplicates(&mut vec![]));
+        }
+    }
+}
--- src/reverse_nodes_in_k_group.rs
+++ src/reverse_nodes_in_k_group.rs
@@ -1,45 +1,55 @@
 pub mod solution_iterative {
     use crate::helper::*;
     /// # 思路
-    /// 
+    ///
     /// 链表分区为已翻转部分+待翻转部分+未翻转部分
-    /// 
+    ///
     /// 翻转类似[reverse_linked_list](https://leetcode-cn.com/problems/reverse-linked-list/)，
     /// 通过pre节点找出第k个tail节点，切断反转并重建链表
     /// 
-    /// ![](https://pic.leetcode-cn.com/866b404c6b0b52fa02385e301ee907fc015742c3766c80c02e24ef3a8613e5ad-k%E4%B8%AA%E4%B8%80%E7%BB%84%E7%BF%BB%E8%BD%AC%E9%93%BE%E8%A1%A8.png)
-    /// 
     /// ```java
-    /// // 还有改进的地步，像递归一样
+    /// // 移除
+    ///             // 切断链表
+    /// head = pre.next;
+    /// ListNode next = tail.next;
+    /// tail.next = null;
+    /// ```
+    /// 
+    /// 改进为不再先切断tail，而是用reverse head与nextHead关系会head.next=null切断，再用
+    /// head.next = nextHead链接
+    ///
+    /// ![](https://pic.leetcode-cn.com/866b404c6b0b52fa02385e301ee907fc015742c3766c80c02e24ef3a8613e5ad-k%E4%B8%AA%E4%B8%80%E7%BB%84%E7%BF%BB%E8%BD%AC%E9%93%BE%E8%A1%A8.png)
+    ///
+    /// ```java
+    /// // 改进为递归
     /// @Submission(date = "20200829", memory = 39.9, memoryBeatRate = 54, runtime = 1, runtimeBeatRate = 35, url = "https://leetcode-cn.com/submissions/detail/102745511/")
+    /// @Submission(date = "20200830", memory = 40.1, memoryBeatRate = 17.08, runtime = 0, runtimeBeatRate = 100, url = "https://leetcode-cn.com/submissions/detail/103021922/")
+    /// //iterative
     /// public ListNode reverseKGroup(ListNode head, int k) {
     ///     ListNode dummy = new ListNode(0);
     ///     dummy.next = head;
     ///     ListNode pre = dummy;
-    ///     while (head != null) {
-    ///         ListNode tail = pre;
-    ///         // 找到第k个节点tail
+    ///     while (true) {
+    ///         // find next head in k
+    ///         head = pre.next;
+    ///         ListNode nextHead = head;
     ///         for (int i = 0; i < k; i++) {
-    ///             if ((tail = tail.next) == null) {
+    ///             if (nextHead == null) {
     ///                 return dummy.next;
     ///             }
+    ///             nextHead = nextHead.next;
     ///         }
-    ///         // 切断链表
-    ///         head = pre.next;
-    ///         ListNode next = tail.next;
-    ///         tail.next = null;
-    ///         // head is tail
-    ///         pre.next = reverseList(head);
-    ///         // 重建 
-    ///         head.next = next;
+    ///         // reverse
+    ///         pre.next = reverse(head, nextHead);
+    ///         // link reversed. now head is tail
+    ///         head.next = nextHead;
     ///         pre = head;
     ///     }
-    ///     return dummy.next;
     /// }
-    ///
-    /// private ListNode reverseList(ListNode head) {
+    /// 
+    /// private ListNode reverse(ListNode head, ListNode nextHead) {
     ///     ListNode prev = null;
-    ///     while (head != null) {
+    ///     while (head != nextHead) {
     ///         ListNode next = head.next;
     ///         head.next = prev;
     ///         prev = head;
@@ -48,20 +58,22 @@ pub mod solution_iterative {
     ///     return prev;
     /// }
     /// ```
-    /// 
+    ///
     /// 参考：
-    /// 
+    ///
     /// - [图解k个一组翻转链表](https://leetcode-cn.com/problems/reverse-nodes-in-k-group/solution/tu-jie-kge-yi-zu-fan-zhuan-lian-biao-by-user7208t/)
     /// - [K 个一组翻转链表](https://leetcode-cn.com/problems/reverse-nodes-in-k-group/solution/k-ge-yi-zu-fan-zhuan-lian-biao-by-leetcode-solutio/)
-    /// 
+    ///
     /// ### 复杂度
-    /// 
+    ///
     /// - 时间：O(n)
     /// - 空间：O(1)
     pub struct Solution;
 
-    pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
-        todo!()
+    impl Solution {
+        pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+            todo!()
+        }
     }
 }
 
@@ -69,13 +81,14 @@ pub mod solution_recursive {
     use crate::helper::*;
 
     /// # 思路
-    /// 
+    ///
     /// 先反转每段k个链表，递归回溯时连接相邻链表
-    /// 
+    ///
     /// ![](https://pic.leetcode-cn.com/f63d5ca4d3f055ce8e4591c8bc51c288791f88da9ccec9617bc8bb51c26163a2.png)
-    /// 
+    ///
     /// ```java
     /// @Submission(date = "20200829", memory = 40.2, memoryBeatRate = 8.88, runtime = 0, runtimeBeatRate = 100, url = "https://leetcode-cn.com/submissions/detail/102757084/")
+    /// @Submission(date = "20200830", memory = 40.1, memoryBeatRate = 17.84, runtime = 0, runtimeBeatRate = 100, url = "https://leetcode-cn.com/submissions/detail/103019337/")
     /// public ListNode reverseKGroup(ListNode head, int k) {
     ///     if (head == null) {
     ///         return null;
@@ -92,7 +105,7 @@ pub mod solution_recursive {
     ///     head.next = reverseKGroup(nextHead, k);
     ///     return newHead;
     /// }
-    /// 
+    ///
     /// // 反转head->nextHead不包括nextHead
     /// private ListNode reverse(ListNode head, ListNode nextHead) {
     ///     ListNode prev = null;
@@ -105,19 +118,21 @@ pub mod solution_recursive {
     ///     return prev;
     /// }
     /// ```
-    /// 
+    ///
     /// 参考：
-    /// 
+    ///
     /// - [递归思维：如何跳出细节？](https://leetcode-cn.com/problems/reverse-nodes-in-k-group/solution/di-gui-si-wei-ru-he-tiao-chu-xi-jie-by-labuladong/)
     /// - [递归java](https://leetcode-cn.com/problems/reverse-nodes-in-k-group/solution/di-gui-java-by-reedfan-2/)
-    /// 
+    ///
     /// ### 复杂度
-    /// 
+    ///
     /// - 时间：O(n)
     /// - 空间：O(log n)
     pub struct Solution;
 
-    pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
-        todo!()
+    impl Solution {
+        pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+            todo!()
+        }
     }
 }
