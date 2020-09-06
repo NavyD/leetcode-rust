3ebeee6ea6d3e412bcf8164eb280d9714c67aba1
navyd <dhjnavyd@gmail.com>
2020-09-06 11:03:08 +0800

    reverse_nodes_in_k_group third. largest_rectangle_in_histogram violent width,height

--- src/largest_rectangle_in_histogram.rs
+++ src/largest_rectangle_in_histogram.rs
@@ -1,3 +1,116 @@
+pub mod solution_violent_width {
+    /// # 思路
+    /// 
+    /// 枚举「宽」，我们可以使用两重循环枚举矩形的左右边界以固定宽度 w，
+    /// 此时矩形的高度 h，就是所有包含在内的柱子的「最小高度」，对应的面积为 w * h
+    /// 
+    /// 参考：
+    /// 
+    /// - [柱状图中最大的矩形](https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/zhu-zhuang-tu-zhong-zui-da-de-ju-xing-by-leetcode-/)
+    /// 
+    /// ### Submissions
+    /// 
+    /// date=20200906, mem=2.2, mem_beats=87.50, runtime=684, runtime_beats=5.97, url=https://leetcode-cn.com/submissions/detail/105186410/
+    /// 
+    /// ### 复杂度
+    /// 
+    /// - 时间：O(n^2)
+    /// - 空间：O(1)
+    pub struct Solution;
+
+    impl Solution {
+        pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
+            let mut max_area = 0;
+            for left in 0..heights.len() {
+                let mut min_height = heights[left];
+                // 获取右边最小高度 对应的width
+                for right in left..heights.len() {
+                    let width = (right - left + 1) as i32;
+                    min_height = min_height.min(heights[right]);
+                    max_area = max_area.max(min_height * width);
+                }
+            }
+            max_area
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn basics() {
+            assert_eq!(10, Solution::largest_rectangle_area(vec![2, 1, 5, 6, 2, 3]));
+            assert_eq!(0, Solution::largest_rectangle_area(vec![]));
+            assert_eq!(5, Solution::largest_rectangle_area(vec![5]));
+            assert_eq!(5, Solution::largest_rectangle_area(vec![2, 5]));
+            assert_eq!(2, Solution::largest_rectangle_area(vec![1, 1]));
+        }
+    }
+}
+
+pub mod solution_violent_height {
+    /// # 思路
+    /// 
+    /// 枚举以每个柱形为高度的最大矩形的面积，依次遍历柱形的高度，
+    /// 对于每一个高度分别向两边扩散，求出以当前高度为矩形的最大宽度多少
+    /// 
+    /// - 左边看一下，看最多能向左延伸多长，找到大于等于当前柱形高度的最左边元素的下标；
+    /// - 右边看一下，看最多能向右延伸多长；找到大于等于当前柱形高度的最右边元素的下标。
+    /// 
+    /// ![](https://pic.leetcode-cn.com/b4125f95419bc2306c7f16d1679c32e538b0b087bd9d0f70658c1a8528afca6b-image.png)
+    /// 
+    /// 参考：
+    /// 
+    /// - [暴力解法、栈（单调栈、哨兵技巧）](https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/bao-li-jie-fa-zhan-by-liweiwei1419/)
+    /// - [柱状图中最大的矩形](https://leetcode-cn.com/problems/largest-rectangle-in-histogram/solution/zhu-zhuang-tu-zhong-zui-da-de-ju-xing-by-leetcode-/)
+    /// 
+    /// ### Submissions
+    /// 
+    /// date=20200906, mem=2.2, mem_beats=93.75, runtime=880, runtime_beats=5.97, url=https://leetcode-cn.com/submissions/detail/105190091/
+    /// 
+    /// ### 复杂度
+    /// 
+    /// - 时间：O(n^2)
+    /// - 空间：O(1)
+    pub struct Solution;
+
+    impl Solution {
+        pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
+            let mut max_area = 0;
+            for mid in 0..heights.len() {
+                let height = heights[mid];
+                let (mut left, mut right) = (mid, mid);
+                // 向左边找 >= height
+                while left > 0 && heights[left - 1] >= height {
+                    left -= 1;
+                }
+                // 向右边找 >= height
+                while right < heights.len() - 1 && heights[right + 1] >= height {
+                    right += 1;
+                }
+                let width = (right - left + 1) as i32;
+                max_area = max_area.max(width * height);
+            }
+            max_area
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn basics() {
+            assert_eq!(10, Solution::largest_rectangle_area(vec![2, 1, 5, 6, 2, 3]));
+            assert_eq!(0, Solution::largest_rectangle_area(vec![]));
+            assert_eq!(5, Solution::largest_rectangle_area(vec![5]));
+            assert_eq!(5, Solution::largest_rectangle_area(vec![2, 5]));
+            assert_eq!(2, Solution::largest_rectangle_area(vec![1, 1]));
+        }
+    }
+}
+
 pub mod solution_double_pointer {
     /// 如何解决中间的最小值
     pub struct Solution;
@@ -33,7 +146,7 @@ pub mod solution_double_pointer {
             assert_eq!(0, Solution::largest_rectangle_area(vec![]));
             assert_eq!(5, Solution::largest_rectangle_area(vec![5]));
             assert_eq!(5, Solution::largest_rectangle_area(vec![2, 5]));
-            assert_eq!(2, Solution::largest_rectangle_area(vec![1,1]));
+            assert_eq!(2, Solution::largest_rectangle_area(vec![1, 1]));
         }
     }
 }
@@ -84,7 +197,7 @@ pub mod solution_monotonous_stack {
         pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
             if heights.len() == 0 {
                 return 0;
-            } 
+            }
             if heights.len() == 1 {
                 return heights[0];
             }
@@ -96,7 +209,7 @@ pub mod solution_monotonous_stack {
                     if height >= last_height {
                         break;
                     }
-                    // height < last_height 
+                    // height < last_height
                     // find the last == last_height
                     while let Some(last) = last_indices.pop() {
                         if last_height != &heights[last] {
@@ -142,8 +255,67 @@ pub mod solution_monotonous_stack {
             assert_eq!(0, Solution::largest_rectangle_area(vec![]));
             assert_eq!(5, Solution::largest_rectangle_area(vec![5]));
             assert_eq!(5, Solution::largest_rectangle_area(vec![2, 5]));
-            assert_eq!(2, Solution::largest_rectangle_area(vec![1,1]));
+            assert_eq!(2, Solution::largest_rectangle_area(vec![1, 1]));
+        }
+    }
+}
+
+pub mod solution_stack_sentinel {
+
+    pub struct Solution;
+    impl Solution {
+        pub fn largest_rectangle_area(mut heights: Vec<i32>) -> i32 {
+            heights.push(0);
+            let mut last_indices = Vec::with_capacity(heights.len());
+            let mut max_area = 0;
+            for (i, height) in heights.iter().enumerate() {
+                while let Some(last) = last_indices.last() {
+                    if height < &heights[*last] {
+                        last_indices.pop();
+                        let width = if let Some(last) = last_indices.last() {
+                            i - last - 1
+                        } else {
+                            i
+                        };
+                        max_area = max_area.max(width as i32 * height);
+                    }
+                }
+                last_indices.push(i);
+            }
+            max_area
+        }
+
+        pub fn largest_rectangle_area1(heights: Vec<i32>) -> i32 {
+            let mut stack = vec![];
+            let mut res = 0;
+            for i in 0..=heights.len() {
+                let cur_height = if i == heights.len() { 0 } else { heights[i] };
+
+                while !stack.is_empty() && cur_height < heights[*stack.last().unwrap()] {
+                    let h = stack.pop().unwrap();
+                    if stack.is_empty() {
+                        res = res.max(i as i32 * heights[h])
+                    } else {
+                        res = res.max((i - *stack.last().unwrap() - 1) as i32 * heights[h]);
+                    }
+                }
+                stack.push(i)
+            }
+            res
         }
     }
 
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn basics() {
+            // assert_eq!(10, Solution::largest_rectangle_area(vec![2,1,5,6,2,3]));
+            assert_eq!(0, Solution::largest_rectangle_area(vec![]));
+            assert_eq!(5, Solution::largest_rectangle_area(vec![5]));
+            assert_eq!(5, Solution::largest_rectangle_area(vec![2, 5]));
+            assert_eq!(2, Solution::largest_rectangle_area(vec![1, 1]));
+        }
+    }
 }
--- src/reverse_nodes_in_k_group.rs
+++ src/reverse_nodes_in_k_group.rs
@@ -24,14 +24,14 @@ pub mod solution_iterative {
     /// // 改进为递归
     /// @Submission(date = "20200829", memory = 39.9, memoryBeatRate = 54, runtime = 1, runtimeBeatRate = 35, url = "https://leetcode-cn.com/submissions/detail/102745511/")
     /// @Submission(date = "20200830", memory = 40.1, memoryBeatRate = 17.08, runtime = 0, runtimeBeatRate = 100, url = "https://leetcode-cn.com/submissions/detail/103021922/")
+    /// @Submission(date = "20200906", memory = 39.8, memoryBeatRate = 76.54, runtime = 0, runtimeBeatRate = 100, url = "https://leetcode-cn.com/submissions/detail/105201958/")
     /// //iterative
     /// public ListNode reverseKGroup(ListNode head, int k) {
     ///     ListNode dummy = new ListNode(0);
     ///     dummy.next = head;
-    ///     ListNode pre = dummy;
-    ///     while (true) {
+    ///     ListNode tail = dummy;
+    ///     while (head != null) {
     ///         // find next head in k
-    ///         head = pre.next;
     ///         ListNode nextHead = head;
     ///         for (int i = 0; i < k; i++) {
     ///             if (nextHead == null) {
@@ -40,11 +40,13 @@ pub mod solution_iterative {
     ///             nextHead = nextHead.next;
     ///         }
     ///         // reverse
-    ///         pre.next = reverse(head, nextHead);
+    ///         tail.next = reverse(head, nextHead);
     ///         // link reversed. now head is tail
-    ///         head.next = nextHead;
-    ///         pre = head;
+    ///         tail = head;
+    ///         tail.next = nextHead;
+    ///         head = nextHead;
     ///     }
+    ///     return dummy.next;
     /// }
     ///
     /// private ListNode reverse(ListNode head, ListNode nextHead) {
@@ -72,6 +74,7 @@ pub mod solution_iterative {
 
     impl Solution {
         #[allow(unused_variables)]
+        /// iterative: https://github.com/Aloxaf/LeetCode-Rust/blob/master/src/reverse_nodes_in_k_group.rs
         pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
             todo!()
         }
@@ -86,21 +89,26 @@ pub mod solution_recursive {
     /// rust所有权的方式不能在递归前反转链表，只能递归后返回反转的new_head作为next_head
     /// 与上个head反转链接 
     /// 
-    /// `head -> ... -x-> next_head -> tail` 反转链接 `tail <- head <- ... <- next_head` 并返回next_head
+    /// `head -> ... -> tail -X-> next_head -> ...` 反转链接 `... <- next_head <- head <- ... <- tail` 并返回new_head(tail)
     /// 
     /// ```rust,ignore
     /// Solution::reverse(head, next_head)
-    /// // cannot move out of `next_head` 
+    /// // error: move out of `next_head` 
     /// let next_head = Solution::reverse_k_group(next_head.take(), k);
     /// ```
     ///
-    /// 先反转每段k个链表，递归回溯时连接相邻链表
-    ///
+    /// java先反转每段k个链表，递归回溯时连接相邻链表.
+    /// 
+    /// 为何要先判断`nextHead == null`，再`nextHead = nextHead.next;`,而不用`if ((nextHead = nextHead.next) == null)`？
+    /// 
+    /// k次循环中在最后一次时可允许nextHead为null，而(nextHead = nextHead.next) == null不会执行第k次
+    /// 
     /// ![](https://pic.leetcode-cn.com/f63d5ca4d3f055ce8e4591c8bc51c288791f88da9ccec9617bc8bb51c26163a2.png)
     ///
     /// ```java
     /// @Submission(date = "20200829", memory = 40.2, memoryBeatRate = 8.88, runtime = 0, runtimeBeatRate = 100, url = "https://leetcode-cn.com/submissions/detail/102757084/")
     /// @Submission(date = "20200830", memory = 40.1, memoryBeatRate = 17.84, runtime = 0, runtimeBeatRate = 100, url = "https://leetcode-cn.com/submissions/detail/103019337/")
+    /// @Submission(date = "20200906", memory = 40.1, memoryBeatRate = 21.02, runtime = 0, runtimeBeatRate = 100, url = "https://leetcode-cn.com/submissions/detail/105196990/")
     /// public ListNode reverseKGroup(ListNode head, int k) {
     ///     if (head == null) {
     ///         return null;
@@ -112,9 +120,9 @@ pub mod solution_recursive {
     ///         }
     ///         nextHead = nextHead.next;
     ///     }
-    ///     ListNode newHead = reverse(head, nextHead);
     ///     // head is tail
     ///     head.next = reverseKGroup(nextHead, k);
+    ///     ListNode newHead = reverse(head, nextHead);
     ///     return newHead;
     /// }
     ///
@@ -136,6 +144,12 @@ pub mod solution_recursive {
     /// - [递归思维：如何跳出细节？](https://leetcode-cn.com/problems/reverse-nodes-in-k-group/solution/di-gui-si-wei-ru-he-tiao-chu-xi-jie-by-labuladong/)
     /// - [递归java](https://leetcode-cn.com/problems/reverse-nodes-in-k-group/solution/di-gui-java-by-reedfan-2/)
     /// - [4ms](https://leetcode.com/submissions/api/detail/25/rust/4/)
+    /// 
+    /// ### Submissions
+    /// 
+    /// date=20200906, mem=2.3, mem_beats=36, runtime=4, runtime_beats=49, url=https://leetcode-cn.com/submissions/detail/103051900/
+    /// 
+    /// date=20200906, mem=2.3, mem_beats=64.29, runtime=0, runtime_beats=100, url=https://leetcode-cn.com/submissions/detail/105213328/
     ///
     /// ### 复杂度
     ///
@@ -159,14 +173,16 @@ pub mod solution_recursive {
             Solution::reverse(head, next_head)
         }
         
-        /// head -> ... -x-> next_head -> tail 反转链接 tail <- head <- ... <- next_head 并返回new_head
+        // head -> ... -> tail -x-> next_head -> ... 反转链接 ... <- next_head <- head <- ... <- tail 并返回new_head(tail)
         fn reverse(
             mut head: Option<Box<ListNode>>,
             mut next_head: Option<Box<ListNode>>,
         ) -> Option<Box<ListNode>> {
             while let Some(mut node) = head {
                 head = node.next.take();
+                // link head -> next_head
                 node.next = next_head.take();
+                // as tail
                 next_head = Some(node);
             }
             next_head
