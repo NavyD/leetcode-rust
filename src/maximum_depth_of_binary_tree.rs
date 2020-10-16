bed5fd94fdcceac6de8614c2da31ee077a5e1033
navyd <dhjnavyd@gmail.com>
2020-10-16 10:33:24 +0800

    daily task for Maximum Depth of Binary Tree

--- src/maximum_depth_of_binary_tree.rs
+++ src/maximum_depth_of_binary_tree.rs
@@ -1,51 +1,41 @@
-// Definition for a binary tree node.
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-  pub val: i32,
-  pub left: Option<Rc<RefCell<TreeNode>>>,
-  pub right: Option<Rc<RefCell<TreeNode>>>,
-}
+//! 如何计算树深度
+//! 
+//! dfs递归回溯统计或bfs层层遍历
 
-impl TreeNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
-    }
-  }
-}
-use std::rc::Rc;
+use crate::prelude::*;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 pub mod solution_dfs {
     use super::*;
     /// # 思路
+    ///
+    /// 递归回溯时计算出深度
     /// 
-    /// 递归计算出深度
-    /// 
+    /// - 当root=None时终止返回0
+    /// - 计算root.left子树与root.right子树最大深度max_depth
+    /// - root层尝试是子树深度`max_depth+1`
+    ///
     /// ## Submissions
-    /// 
+    ///
     /// date=20200713, mem=2.7, mem_beats=80.95, runtime=0, runtime_beats=100, url=https://leetcode.com/submissions/detail/365857015/
     /// 
+    /// date=20201016, mem=2.5, mem_beats=92, runtime=0, runtime_beats=100, url=https://leetcode-cn.com/submissions/detail/116131087/
+    ///
     /// ## 复杂度
-    /// 
+    ///
     /// - 时间：O(N)
     /// - 空间：O(log N)
-    /// 
+    ///
     pub struct Solution;
-    
+
     impl Solution {
         pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-            if let None = root {
-                return 0;
-            } else {
-                let root = root.to_owned().unwrap();
-                let left_count = Self::max_depth(root.borrow().left.clone()) + 1;
-                let right_count = Self::max_depth(root.borrow().right.clone()) + 1;
-                return left_count.max(right_count)
-            }
+            root.map_or(0, |root| {
+                Self::max_depth(root.borrow().left.clone())
+                    .max(Self::max_depth(root.borrow().right.clone()))
+                    + 1
+            })
         }
     }
 }
@@ -53,48 +43,67 @@ pub mod solution_dfs {
 pub mod solution_bfs {
     use super::*;
     /// # 思路
-    /// 
+    ///
     /// 一层一层向下计算
-    /// 
+    ///
     /// 如何遍历平级结点？
-    /// 
+    ///
     /// 注意在平级结点中存在后面子结点push_back，不能pop出子结点作为平级结点用，
     /// 应该用size个数限制
     /// 
+    /// 参考：
+    /// 
+    ///- [My code of C++, Depth-first-search and Breadth-first-search](https://leetcode.com/problems/maximum-depth-of-binary-tree/discuss/34207/My-code-of-C%2B%2B-Depth-first-search-and-Breadth-first-search)
+    ///
     /// ## Submissions
-    /// 
+    ///
     /// date=20200713, mem=2.5， mem_beats=100, runtime=0, runtime_beats=100, url=https://leetcode.com/submissions/detail/365882616/
-    /// 
-    /// author=makuiyu, references=https://leetcode.com/problems/maximum-depth-of-binary-tree/discuss/34207/My-code-of-C%2B%2B-Depth-first-search-and-Breadth-first-search
-    /// 
+    ///
+    /// date=20201016, mem=2.6， mem_beats=82.72, runtime=0, runtime_beats=100, url=https://leetcode-cn.com/submissions/detail/116132950/
+    ///
     /// ## 复杂度
-    /// 
+    ///
     /// - 时间：O(N)
     /// - 空间：O(N)
     pub struct Solution;
-    
+
     impl Solution {
         pub fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-            use std::collections::VecDeque;
-            let mut queue = VecDeque::new();
-            queue.push_back(root);
-            let mut count = 0;
-            while let Some(Some(_)) = queue.front() {
-                count += 1;
-                for _ in 0..queue.len() {
-                    // pop_front保证不将push_back的子结点作为平级结点计算
-                    if let Some(Some(node)) = queue.pop_front() {
-                        let node = node.borrow();
-                        if node.left.is_some() {
-                            queue.push_back(node.left.to_owned());
+            root.map_or(0, |root| {
+                let mut queue = std::collections::VecDeque::new();
+                queue.push_back(root);
+                let mut level = 0;
+                while !queue.is_empty() {
+                    for _ in 0..queue.len() {
+                        let root = queue.pop_front().unwrap();
+                        let root = root.borrow();
+                        if let Some(left) = root.left.clone() {
+                            queue.push_back(left);
                         }
-                        if node.right.is_some() {
-                            queue.push_back(node.right.to_owned());
+                        if let Some(right) = root.right.clone() {
+                            queue.push_back(right);
                         }
                     }
+                    level += 1;
                 }
-            }
-            count
+                level
+            })
         }
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basics() {
+        test(solution_bfs::Solution::max_depth);
+        test(solution_dfs::Solution::max_depth);
+    }
+
+    fn test<F: Fn(Option<Rc<RefCell<TreeNode>>>) -> i32>(func: F) {
+        assert_eq!(3, func(btree![3, 9, 20, null, null, 15, 7]));
     }
 }
--- src/validate_binary_search_tree.rs
+++ src/validate_binary_search_tree.rs
@@ -1,49 +1,55 @@
 //! 如何递归比较root,left,right的大小
-//! 
-//! 在第一次写时，只想到用root.val与left,right比较，没有更多思考，递归条件何时返回，如何处理都没有理清
-//!
-//! ```rust,ignore
-//! pub fn is_valid_bst_error(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
-//!     if let Some(root) = root {
-//!         let root = root.borrow();
-//!         if let Some(left) = root.left.clone() {
-//!             return if left.borrow().val < root.val {
-//!                 Self::is_valid_bst(Some(left))
-//!             } else {
-//!                 false
-//!             };
-//!         }
-//!         if let Some(right) = root.right.clone() {
-//!             return if right.borrow().val > root.val {
-//!                 Self::is_valid_bst(Some(right))
-//!             } else {
-//!                 false
-//!             };
-//!         }
-//!     }
-//!     true
-//! }
-//! ```
 
 use crate::prelude::*;
 use std::cell::RefCell;
 use std::rc::Rc;
 
+/// 在第一次写时，只想到用root.val与left,right比较，没有更多思考，递归条件何时返回，如何处理都没有理清
+///
+/// ```rust,ignore
+/// pub fn is_valid_bst_error(root: Option<Rc<RefCell<TreeNode>>>) -> bool {
+///     if let Some(root) = root {
+///         let root = root.borrow();
+///         if let Some(left) = root.left.clone() {
+///             return if left.borrow().val < root.val {
+///                 Self::is_valid_bst(Some(left))
+///             } else {
+///                 false
+///             };
+///         }
+///         if let Some(right) = root.right.clone() {
+///             return if right.borrow().val > root.val {
+///                 Self::is_valid_bst(Some(right))
+///             } else {
+///                 false
+///             };
+///         }
+///     }
+///     true
+/// }
+/// ```
 pub mod solution_dfs {
     use super::*;
 
     /// # 思路
     ///
+    /// 要验证二叉搜索树是有序的，只要像数组一样检查是否满足：前一个值 < 当前值。而中序遍历是有序的
+    ///
     /// 中序遍历时，判断当前节点是否大于中序遍历的前一个节点，如果大于，说明满足 BST，继续遍历；否则直接返回 false。
-    /// 
+    ///
     /// 注意`pre_val: std::i64::MIN`是比`root.val: i32`更小的，不能设为`std::i32::MIN`
-    /// 
+    ///
     /// 中序node与pre_node每一步对应关系：
-    /// 
+    ///
     /// ```ignore
     /// node:       left -> root -> right
     /// pre_node:   None -> left -> root
     /// ```
+    ///
+    /// - 当root=None时终止返回true。单root一定是有序的
+    /// - 当left子树是无序是返回false
+    /// - 如果root.val <= pre_val则返回false，否则更新pre_val=root.val
+    /// - 返回right子树结果
     ///
     /// 参考：
     ///
@@ -52,6 +58,8 @@ pub mod solution_dfs {
     /// ### Submissions
     ///
     /// date=20201015, mem=3.1, mem_beats=5, runtime=0, runtime_beats=100, url=https://leetcode-cn.com/submissions/detail/115899425/
+    ///
+    /// date=20201016, mem=3.2, mem_beats=7.41, runtime=0, runtime_beats=100, url=https://leetcode-cn.com/submissions/detail/116145829/
     ///
     pub struct Solution;
     impl Solution {
@@ -62,10 +70,7 @@ pub mod solution_dfs {
         pub fn valid_bst(root: Option<Rc<RefCell<TreeNode>>>, pre_val: &mut i64) -> bool {
             root.map_or(true, |root| {
                 let root = root.borrow();
-                if !Self::valid_bst(root.left.clone(), pre_val) {
-                    return false;
-                }
-                if root.val as i64 <= *pre_val {
+                if !Self::valid_bst(root.left.clone(), pre_val) || root.val as i64 <= *pre_val {
                     return false;
                 }
                 *pre_val = root.val as i64;
@@ -75,18 +80,32 @@ pub mod solution_dfs {
     }
 }
 
+/// 20201016
+/// 第二次 注意不能让`root = node.borrow().right.clone()`用下面代码替换，
+/// 还有`while root.is_some() || !stack.is_empty()`条件不能忘记
+/// 
+/// ```rust, ignore
+/// while !stack.is_empty() {
+///     if let Some(right) = node.borrow().right.clone() {
+///         stack.push(right);
+///     }
+///     // root = node.borrow().right.clone();
+/// }
+/// ```
 pub mod solution_dfs_iterative {
     use super::*;
 
     /// # 思路
-    /// 
+    ///
     /// 是[solution_dfs::Solution](../solution_dfs/struct.Solution.html)的迭代写法
     ///
     /// [inorder写法](../../binary_tree_inorder_traversal/solution_stack/struct.Solution.html)
     ///
     /// ### Submissions
     ///
-    /// date=20201015, mem=3.1, mem_beats=100, runtime=100, runtime_beats=100, url=https://leetcode.com/submissions/detail/408900690/
+    /// date=20201015, mem=3.1, mem_beats=100, runtime=0, runtime_beats=100, url=https://leetcode.com/submissions/detail/408900690/
+    /// 
+    /// date=20201016, mem=2.7, mem_beats=96.3, runtime=0, runtime_beats=100, url=https://leetcode-cn.com/submissions/detail/116151516/
     pub struct Solution;
     impl Solution {
         pub fn is_valid_bst(mut root: Option<Rc<RefCell<TreeNode>>>) -> bool {
@@ -101,8 +120,8 @@ pub mod solution_dfs_iterative {
                     {
                         return false;
                     }
-                    pre_node = Some(node.clone());
                     root = node.borrow().right.clone();
+                    pre_node = Some(node);
                 }
             }
             true
@@ -124,5 +143,6 @@ mod tests {
         assert!(func(btree![2, 1, 3]));
         assert!(func(btree![-2147483648]));
         assert!(!func(btree![5, 1, 4, null, null, 3, 6]));
+        assert!(!func(btree![10, 5, 15, null, null, 6, 20]));
     }
 }
