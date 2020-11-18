c46c0fa0672e7b272cc508d6c727e19cbd8c48c8
navyd <dhjnavyd@gmail.com>
2020-11-18 18:16:07 +0800

    permutations_ii again

--- src/permutations_ii.rs
+++ src/permutations_ii.rs
@@ -1,34 +1,98 @@
-pub mod solution_used {
+//! 如何对list中的list去重
+//!
+//! - set
+//! - 搜索前判断是否已交换
+//!
+//! 如何对一个list全排列
+//!
+//! - 递归回溯
+
+pub mod solution_sort {
+    /// 为何不能使用&mut nums或再次swap，而是要使用clone
+    ///
     /// ### submissions
-    /// 
-    /// date=20201106, mem=2, mem_beats=70.67, runtime=4, runtime_beats=94, url=https://leetcode-cn.com/submissions/detail/121404268/
+    ///
+    /// date=20201118, mem=2.1, mem_beats=16, runtime=4, runtime_beats=95, url=https://leetcode-cn.com/submissions/detail/124411651/
+    ///                  1,2,2
+    ///         /       |       \
+    ///        1        2        x
+    ///     2    x    1  2
+    ///  2           2    1
+    ///
     pub struct Solution;
 
     impl Solution {
         pub fn permute_unique(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
-            let mut used = vec![];
             nums.sort_unstable();
-            nums.iter().for_each(|_| used.push(false));
             let mut res = vec![];
-            Self::helper(&mut nums, 0, &mut used, &mut vec![], &mut res);
+            Self::helper(nums, 0, &mut res);
             res
         }
 
-        fn helper(nums: &[i32], start: usize, used: &mut [bool], path: &mut Vec<i32>, res: &mut Vec<Vec<i32>>) {
-            if path.len() == nums.len() {
-                res.push(path.to_vec());
+        fn helper(mut nums: Vec<i32>, cur_idx: usize, res: &mut Vec<Vec<i32>>) {
+            if cur_idx == nums.len() {
+                res.push(nums);
                 return;
             }
-            for i in 0..nums.len() {
-                if used[i] || (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
-                    continue;
+
+            for i in cur_idx..nums.len() {
+                if i == cur_idx || &nums[i] != &nums[cur_idx] {
+                    nums.swap(i, cur_idx);
+                    Self::helper(nums.clone(), cur_idx + 1, res);
                 }
-                path.push(nums[i]);
-                used[i] = true;
-                Self::helper(nums, start + 1, used, path, res);
-                used[i] = false;
-                path.pop();
             }
         }
+    }
+}
+
+pub mod solution_set {
+    use std::collections::HashSet;
+
+    ///                     1,2,3
+    ///         /               |                   \
+    ///        1                2                   3
+    ///       / \
+    ///      2   3          1       3           1       2
+    ///     /     \
+    ///    3       2    3               1   2               1
+    ///
+    /// ### Submissions
+    ///
+    /// date=20201118, mem=2.1, mem_beats=46, runtime=20, runtime_beats=13, url=https://leetcode-cn.com/submissions/detail/124379964/
+    pub struct Solution;
+
+    impl Solution {
+        pub fn permute_unique(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+            let mut set = HashSet::new();
+            Self::helper(&mut nums, 0, &mut set);
+            set.into_iter().collect()
+        }
+
+        fn helper(nums: &mut Vec<i32>, cur_idx: usize, set: &mut HashSet<Vec<i32>>) {
+            if nums.len() <= cur_idx {
+                set.insert(nums.to_vec());
+                return;
+            }
+
+            for i in cur_idx..nums.len() {
+                nums.swap(cur_idx, i);
+                Self::helper(nums, cur_idx + 1, set);
+                nums.swap(cur_idx, i);
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basics() {
+        test(solution_sort::Solution::permute_unique);
+    }
+
+    fn test<F: Fn(Vec<i32>) -> Vec<Vec<i32>>>(func: F) {
+        func(vec![1, 2, 2]);
     }
 }
