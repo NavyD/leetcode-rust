4fa15b21b960407b6c0fb078648ccae9fc4204aa
navyd <dhjnavyd@gmail.com>
2022-05-08 11:27:16 +0800

    added solution union find

--- src/number_of_provinces.rs
+++ src/number_of_provinces.rs
@@ -1,6 +1,9 @@
 pub mod solution_dfs {
     /// # 思路
     ///
+    /// 注意dfs时搜索到一个城市时，下一次从当前城市开始搜索下一个相连城市，
+    /// 所以要传入i `fn dfs(is_connected: &[Vec<i32>], i: usize, visited: &mut [bool])`
+    ///
     /// 参考：
     ///
     /// * [Neat DFS java solution](https://leetcode.com/problems/number-of-provinces/discuss/101338/Neat-DFS-java-solution)
@@ -8,6 +11,8 @@ pub mod solution_dfs {
     /// ### Submissions
     ///
     /// date=20220415, mem=2.2, mem_beats=100, runtime=0, runtime_beats=100
+    ///
+    /// date=20220508, mem=2.2, mem_beats=33, runtime=0, runtime_beats=100
     pub struct Solution;
 
     impl Solution {
@@ -21,10 +26,11 @@ pub mod solution_dfs {
                 }
             }
 
-            let n = is_connected.len();
+            let cities = is_connected.len();
             let mut count = 0;
-            let mut visited = vec![false; n];
-            for i in 0..n {
+            let mut visited = vec![false; cities];
+
+            for i in 0..cities {
                 if !visited[i] {
                     dfs(&is_connected, i, &mut visited);
                     count += 1;
@@ -45,30 +51,97 @@ pub mod solution_bfs {
     /// ### Submissions
     ///
     /// date=20220416, mem=2.2, mem_beats=50, runtime=0, runtime_beats=100
+    ///
+    /// date=20220508, mem=2.3, mem_beats=33, runtime=4, runtime_beats=55
     pub struct Solution;
 
     impl Solution {
         pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
-            let n = is_connected.len();
-
+            let cities = is_connected.len();
             let mut provinces = 0;
-            let mut visited = vec![false; n];
-            let mut queue = std::collections::VecDeque::new();
-            for i in 0..n {
+
+            let mut visited = vec![false; cities];
+            let mut queue = std::collections::VecDeque::default();
+
+            for i in 0..cities {
                 if !visited[i] {
                     queue.push_back(i);
-                    while let Some(j) = queue.pop_front() {
-                        for k in 0..n {
-                            if is_connected[j][k] == 1 && !visited[k] {
-                                visited[k] = true;
-                                queue.push_back(k);
+
+                    while let Some(i) = queue.pop_front() {
+                        visited[i] = true;
+
+                        for j in 0..cities {
+                            if is_connected[i][j] == 1 && !visited[j] {
+                                queue.push_back(j);
                             }
                         }
                     }
+
                     provinces += 1;
                 }
             }
             provinces
+        }
+    }
+}
+
+pub mod solution_union {
+    /// # 思路
+    ///
+    /// 参考：
+    ///
+    /// * [DFS + BFS + 并查集，3 种方法计算无向图连通域数量](https://leetcode-cn.com/problems/number-of-provinces/solution/dfs-bfs-bing-cha-ji-3-chong-fang-fa-ji-s-edkl/)
+    /// * [Java solution, Union Find](https://leetcode.com/problems/number-of-provinces/discuss/101336/Java-solution-Union-Find)
+    ///
+    /// ### Submissions
+    ///
+    /// date=20220508, mem=2.1, mem_beats=77, runtime=0, runtime_beats=100
+
+    pub struct Solution;
+
+    impl Solution {
+        pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
+            struct UnionFind {
+                parent: Vec<usize>,
+                count: usize,
+            }
+
+            impl UnionFind {
+                fn new(n: usize) -> Self {
+                    Self {
+                        count: n,
+                        parent: (0..n).into_iter().collect(),
+                    }
+                }
+
+                fn find(&mut self, mut p: usize) -> usize {
+                    while p != self.parent[p] {
+                        self.parent[p] = self.parent[self.parent[p]];
+                        p = self.parent[p];
+                    }
+                    p
+                }
+
+                fn union(&mut self, p: usize, q: usize) {
+                    let (parent_p, parent_q) = (self.find(p), self.find(q));
+                    if parent_p != parent_q {
+                        self.parent[parent_p] = parent_q;
+                        self.count -= 1;
+                    }
+                }
+            }
+
+            let n = is_connected.len();
+            let mut uf = UnionFind::new(n);
+            for i in 0..n {
+                for j in 0..n {
+                    if is_connected[i][j] == 1 {
+                        uf.union(i, j);
+                    }
+                }
+            }
+
+            uf.count as i32
         }
     }
 }
@@ -81,10 +154,20 @@ mod tests {
     fn basics() {
         test(solution_dfs::Solution::find_circle_num);
         test(solution_bfs::Solution::find_circle_num);
+        test(solution_union::Solution::find_circle_num);
     }
 
     fn test<F: Fn(Vec<Vec<i32>>) -> i32>(f: F) {
-        assert_eq!(f(vec![vec![1, 1, 0], vec![1, 1, 0], vec![0, 0, 1]]), 2);
+        assert_eq!(f(vec![vec![1, 1, 0], vec![1, 1, 0], vec![0, 0, 1],]), 2);
         assert_eq!(f(vec![vec![1, 0, 0], vec![0, 1, 0], vec![0, 0, 1]]), 3);
+        assert_eq!(
+            f(vec![
+                vec![1, 0, 0, 1],
+                vec![0, 1, 1, 0],
+                vec![0, 1, 1, 1],
+                vec![1, 0, 1, 1]
+            ]),
+            1
+        );
     }
 }
