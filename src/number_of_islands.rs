e097cf11e136b89f3ff17815818eddf629e8994f
navyd <dhjnavyd@gmail.com>
2022-05-15 12:17:40 +0800

    added union find solution

--- src/number_of_islands.rs
+++ src/number_of_islands.rs
@@ -46,6 +46,40 @@ pub mod solution_dfs {
     /// 标记已经遍历过的格子。以岛屿问题为例，我们需要在所有值为 1 的陆地格子上做 DFS 遍历。
     /// 每走过一个陆地格子，就把格子的值改为 2，这样当我们遇到 2 的时候，就知道这是遍历过的格子了。
     ///
+    /// ## 不修改原数组
+    ///
+    /// ```no_run
+    /// // 8ms
+    /// pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
+    ///     const LAND: char = '1';
+    ///     fn dfs(grid: &[Vec<char>], i: usize, j: usize, visited: &mut [Vec<bool>]) {
+    ///         if i < grid.len() && j < grid[0].len() && grid[i][j] == LAND && !visited[i][j] {
+    ///             visited[i][j] = true;
+    ///             dfs(grid, i + 1, j, visited);
+    ///             dfs(grid, i, j + 1, visited);
+    ///             if i > 0 {
+    ///                 dfs(grid, i - 1, j, visited);
+    ///             }
+    ///             if j > 0 {
+    ///                 dfs(grid, i, j - 1, visited);
+    ///             }
+    ///         }
+    ///     }
+    ///     let (m, n) = (grid.len(), grid[0].len());
+    ///     let mut visited = vec![vec![false; n]; m];
+    ///     let mut count = 0;
+    ///     for i in 0..m {
+    ///         for j in 0..n {
+    ///             if grid[i][j] == LAND && !visited[i][j] {
+    ///                 count += 1;
+    ///                 dfs(&grid, i, j, &mut visited);
+    ///             }
+    ///         }
+    ///     }
+    ///     count
+    /// }
+    /// ```
+    ///
     /// 参考：
     ///
     /// - [岛屿类问题的通用解法、DFS 遍历框架](https://leetcode-cn.com/problems/number-of-islands/solution/dao-yu-lei-wen-ti-de-tong-yong-jie-fa-dfs-bian-li-/)
@@ -57,23 +91,25 @@ pub mod solution_dfs {
     /// date=20210110, mem=5.2, mem_beats=40, runtime=8, runtime_beats=54, url=https://leetcode-cn.com/submissions/detail/137401861/
     ///
     /// date=20210117, mem=5.2, mem_beats=57, runtime=8, runtime_beats=60, url=https://leetcode-cn.com/submissions/detail/139014563/
+    ///
+    /// date=20220515, mem=8.8, mem_beats=72, runtime=12, runtime_beats=72
     pub struct Solution;
 
     impl Solution {
         pub fn num_islands(mut grid: Vec<Vec<char>>) -> i32 {
             const LAND: char = '1';
-            const DELETED_LAND: char = '2';
+            const NON_LAND: char = '2';
 
-            fn _dfs_delete(grid: &mut Vec<Vec<char>>, row: usize, col: usize) {
-                if row < grid.len() && col < grid[row].len() && grid[row][col] == LAND {
-                    grid[row][col] = DELETED_LAND;
-                    _dfs_delete(grid, row + 1, col);
-                    _dfs_delete(grid, row, col + 1);
+            fn dfs(grid: &mut Vec<Vec<char>>, row: usize, col: usize) {
+                if row < grid.len() && col < grid[0].len() && grid[row][col] == LAND {
+                    grid[row][col] = NON_LAND;
+                    dfs(grid, row + 1, col);
+                    dfs(grid, row, col + 1);
                     if row > 0 {
-                        _dfs_delete(grid, row - 1, col);
+                        dfs(grid, row - 1, col);
                     }
                     if col > 0 {
-                        _dfs_delete(grid, row, col - 1);
+                        dfs(grid, row, col - 1);
                     }
                 }
             }
@@ -82,7 +118,7 @@ pub mod solution_dfs {
             for row in 0..grid.len() {
                 for col in 0..grid[row].len() {
                     if grid[row][col] == LAND {
-                        _dfs_delete(&mut grid, row, col);
+                        dfs(&mut grid, row, col);
                         count += 1;
                     }
                 }
@@ -150,6 +186,93 @@ pub mod solution_bfs {
     }
 }
 
+pub mod solution_union {
+    /// # 思路
+    ///
+    /// 注意：
+    ///
+    /// * 只有两个方向`[(1, 0), (0, 1)]`
+    ///
+    /// 对于并查集的做法，第一行向下尝试合并，与第二行向上尝试合并是同一个操作。
+    /// 左边格子和右边格子尝试合并，与右边格子和左边格子尝试合并是同一个操作。
+    /// 所以只用考虑两个方向。
+    ///
+    /// 参考：
+    ///
+    /// - [1D Union Find Java solution, easily generalized to other problems](https://leetcode.com/problems/number-of-islands/discuss/56354/1D-Union-Find-Java-solution-easily-generalized-to-other-problems)
+    /// - [方法三：并查集](https://leetcode.cn/problems/number-of-islands/solution/dfs-bfs-bing-cha-ji-python-dai-ma-java-dai-ma-by-l/)
+    /// - [岛屿数量 方法三：并查集](https://leetcode.cn/problems/number-of-islands/solution/dao-yu-shu-liang-by-leetcode/)
+    ///
+    /// ### Submissions
+    ///
+    /// date=20220515, mem=9, mem_beats=12, runtime=8, runtime_beats=100
+    pub struct Solution;
+
+    impl Solution {
+        pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
+            const LAND: char = '1';
+            const DIRS: [(usize, usize); 2] = [(1, 0), (0, 1)];
+
+            struct UnionFind {
+                count: i32,
+                parent: Vec<usize>,
+                cols: usize,
+            }
+            impl UnionFind {
+                fn new(rows: usize, cols: usize) -> Self {
+                    let count = rows * cols;
+                    Self {
+                        count: count as i32,
+                        parent: (0..count).collect::<Vec<_>>(),
+                        cols,
+                    }
+                }
+
+                fn find(&mut self, mut p: usize) -> usize {
+                    while p != self.parent[p] {
+                        self.parent[p] = self.parent[self.parent[p]];
+                        p = self.parent[p];
+                    }
+                    p
+                }
+
+                fn union(&mut self, p: usize, q: usize) {
+                    let (p, q) = (self.find(p), self.find(q));
+                    if p != q {
+                        self.parent[p] = q;
+                        self.count -= 1;
+                    }
+                }
+
+                #[inline(always)]
+                fn index(&self, row: usize, col: usize) -> usize {
+                    self.cols * row + col
+                }
+            }
+
+            let (rows, cols) = (grid.len(), grid[0].len());
+            let mut uf = UnionFind::new(rows, cols);
+            let mut spaces = 0;
+
+            for i in 0..rows {
+                for j in 0..cols {
+                    if grid[i][j] == LAND {
+                        for (x, y) in DIRS {
+                            let (x, y) = (i + x, j + y);
+                            if x < rows && y < cols && grid[x][y] == LAND {
+                                uf.union(uf.index(i, j), uf.index(x, y));
+                            }
+                        }
+                    } else {
+                        spaces += 1;
+                    }
+                }
+            }
+            uf.count - spaces
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -158,6 +281,7 @@ mod tests {
     fn basic() {
         test(solution_dfs::Solution::num_islands);
         test(solution_bfs::Solution::num_islands);
+        test(solution_union::Solution::num_islands);
     }
 
     fn test<F: Fn(Vec<Vec<char>>) -> i32>(func: F) {
@@ -171,5 +295,20 @@ mod tests {
         .map(|a| a.iter().map(|e| e.chars().next().unwrap()).collect())
         .collect();
         assert_eq!(func(grid), 1);
+
+        let grid = [
+            ["1", "1", "0", "0", "0"],
+            ["1", "1", "0", "0", "0"],
+            ["0", "0", "1", "0", "0"],
+            ["0", "0", "0", "1", "1"],
+        ]
+        .into_iter()
+        .map(|a| {
+            a.into_iter()
+                .flat_map(|e| e.chars().next())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+        assert_eq!(func(grid), 3);
     }
 }
