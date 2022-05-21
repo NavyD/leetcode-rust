8b576e06a2be12c2ed253487daee99d5de84508a
navyd <dhjnavyd@gmail.com>
2022-05-21 21:33:04 +0800

    union find solution

--- src/surrounded_regions.rs
+++ src/surrounded_regions.rs
@@ -58,6 +58,89 @@ pub mod solution_dfs {
     }
 }
 
+pub mod solution_union {
+    pub struct Solution;
+
+    impl Solution {
+        pub fn solve(board: &mut Vec<Vec<char>>) {
+            const O: char = 'O';
+            const X: char = 'X';
+
+            struct UnionFind {
+                parent: Vec<usize>,
+                cols: usize,
+            }
+
+            impl UnionFind {
+                fn new(rows: usize, cols: usize) -> Self {
+                    Self {
+                        parent: (0..rows * cols + 1).collect::<Vec<_>>(),
+                        cols,
+                    }
+                }
+
+                #[inline]
+                fn index(&self, row: usize, col: usize) -> usize {
+                    self.cols * row + col
+                }
+                fn find(&mut self, mut p: usize) -> usize {
+                    while self.parent[p] != p {
+                        self.parent[p] = self.parent[self.parent[p]];
+                        p = self.parent[p];
+                    }
+                    p
+                }
+
+                fn union(&mut self, p: usize, q: usize) {
+                    let (x, y) = (self.parent[p], self.parent[q]);
+                    if x != y {
+                        self.parent[x] = y;
+                    }
+                }
+
+                fn is_unit(&mut self, p: usize, q: usize) -> bool {
+                    self.find(p) == self.find(q)
+                }
+            }
+
+            let (rows, cols) = (board.len(), board[0].len());
+            let mut uf = UnionFind::new(rows, cols);
+            let dummy = rows * cols;
+
+            for i in 0..rows {
+                for j in 0..cols {
+                    if board[i][j] == O {
+                        if i == 0 || j == 0 || i == rows - 1 || j == cols - 1 {
+                            uf.union(uf.index(i, j), dummy);
+                        } else {
+                            if i > 0 && board[i - 1][j] == O {
+                                uf.union(uf.index(i, j), uf.index(i - 1, j));
+                            }
+                            if j > 0 && board[i][j - 1] == O {
+                                uf.union(uf.index(i, j), uf.index(i, j - 1));
+                            }
+                            if i < board.len() - 1 && board[i + 1][j] == O {
+                                uf.union(uf.index(i, j), uf.index(i + 1, j));
+                            }
+                            if j < board[0].len() - 1 && board[i][j + 1] == O {
+                                uf.union(uf.index(i, j), uf.index(i, j + 1));
+                            }
+                        }
+                    }
+                }
+            }
+
+            for i in 0..rows {
+                for j in 0..cols {
+                    if board[i][j] == O && !uf.is_unit(uf.index(i, j), dummy) {
+                        board[i][j] = X;
+                    }
+                }
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -65,6 +148,7 @@ mod tests {
     #[test]
     fn basics() {
         test(solution_dfs::Solution::solve);
+        test(solution_union::Solution::solve);
     }
 
     fn test<F: Fn(&mut Vec<Vec<char>>)>(f: F) {
@@ -98,5 +182,24 @@ mod tests {
         let mut input = arr([["X"]]);
         f(&mut input);
         assert_eq!(input, arr([["X"]]));
+
+        let mut input = arr([
+            ["O", "X", "X", "O", "X"],
+            ["X", "O", "O", "X", "O"],
+            ["X", "O", "X", "O", "X"],
+            ["O", "X", "O", "O", "O"],
+            ["X", "X", "O", "X", "O"],
+        ]);
+        f(&mut input);
+        assert_eq!(
+            input,
+            arr([
+                ["O", "X", "X", "O", "X"],
+                ["X", "X", "X", "X", "O"],
+                ["X", "X", "X", "O", "X"],
+                ["O", "X", "O", "O", "O"],
+                ["X", "X", "O", "X", "O"]
+            ])
+        );
     }
 }
